105a1224b2044ae6444212da3065e9a0075c2285
tmtrbocanegra <tmtrbocanegra@SGGT-PC.TMT.LOCAL>
2018-10-04 09:52:48 -0500

    Primer Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rstinvFrontendApp
+ * @description
+ * # rstinvFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rstinvFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
