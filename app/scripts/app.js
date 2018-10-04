4474da0f2636341281a38a04e6f755363c50b233
tmtrbocanegra <tmtrbocanegra@SGGT-PC.TMT.LOCAL>
2018-10-04 10:54:55 -0500

    Inicializando

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,10 +9,35 @@
  * Main module of the application.
  */
 angular
-  .module('rstinvFrontendApp', [
+.module('rstinvFrontendApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'ui.router'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+    // $httpProvider.interceptors.push('oauthHttpInterceptor');
+    var mainState = {
+        name: 'main',
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main',
+        title: 'Home'
+    };   
+    
+    var crucesState = {
+        name: 'cruces',
+        url: '/cruces',
+        templateUrl: 'views/cruces.html',
+        controller: 'CrucesCtrl',
+        controllerAs: 'cruces',
+        title: 'Cruces'
+    };  
+    
+    $stateProvider.state(mainState);
+    $stateProvider.state(crucesState);
+    $urlRouterProvider.when('', '/');
+});
---
+++ app/scripts/controllers/cruces.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rstinvFrontendApp.controller:CrucesCtrl
+ * @description
+ * # CrucesCtrl
+ * Controller of the rstinvFrontendApp
+ */
+angular.module('rstinvFrontendApp')
+  .controller('CrucesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
